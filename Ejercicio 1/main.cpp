5f966918daf2406c961e22101bfa0a82a1bc69e3
Gmongtor <gmongtor@myuax.com>
2023-11-28 09:20:44 +0100

    Agregamos el itirenador para que recorra el vector.

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -4,10 +4,15 @@
 using namespace std;
 int main(){
     vector<string> nombres;
+
     nombres.push_back("Juan");
     nombres.push_back("Pedro");
     nombres.push_back("Maria");
     nombres.push_back("Jose");
     nombres.push_back("Luis");
 
+    vector<string>::iterator it;
+    for(it = nombres.begin(); it != nombres.end(); it++){
+        cout << *it << endl;
+    }
 }
