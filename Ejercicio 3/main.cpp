ea6852e1faabd0435205558c56d63cbce6c849a3
Gmongtor <gmongtor@myuax.com>
2023-11-28 10:52:23 +0100

    Imprimimos el vector ordenado

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 int main(){
     vector<int> v = {7, 2, 8, 1, 5};
     sort(v.begin(), v.end());
+    cout << "Vector ordenado: ";
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
 
 }
