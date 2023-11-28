0a5b40eb1050b3f8ce98464d64cfa01ea45a5f61
Gmongtor <gmongtor@myuax.com>
2023-11-28 09:18:22 +0100

    Creamos el vector nombres y añadimos al vector los nombres.

---
+++ Ejercicio 1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+    vector<string> nombres;
+    nombres.push_back("Juan");
+    nombres.push_back("Pedro");
+    nombres.push_back("Maria");
+    nombres.push_back("Jose");
+    nombres.push_back("Luis");
+
+}
