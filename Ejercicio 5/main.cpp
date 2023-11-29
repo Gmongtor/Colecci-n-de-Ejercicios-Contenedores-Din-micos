59fc9853384dd75dd3335699310608a4f3b0a0cc
Gmongtor <gmongtor@myuax.com>
2023-11-29 20:52:04 +0100

    commit 5 3

--- Ejercicio 5/main.cpp
+++ Ejercicio 5/main.cpp
@@ -7,5 +7,8 @@ using namespace std;
 int main(){
     auto square = [](int n) { return n * n; };
     auto numeros = std::make_unique<std::vector<int>>(std::vector<int>{1, 2, 3, 4, 5});
-
+    std::for_each(numeros->begin(), numeros->end(), [&square](const auto& n) {
+        std::cout << "Cuadrado de " << n << " es " << square(n) << std::endl;
+    });
+    return 0;
 }
