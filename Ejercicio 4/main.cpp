2f9b3e3675003215cb502f6ca11518238b98880e
Gmongtor <gmongtor@myuax.com>
2023-11-29 20:25:05 +0100

    Añadimos las funciones

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <valarray>
-#include <cmath>
 using namespace std;
 int main(){
-    valarray<int> v1 = {7, 2, 8, 1, 5};
+    valarray<double> valores = {7.3, 2.9, 9.5, 1.7, 3.5};
+
+    valarray<double> suma = valores + 10.0;
+    valarray<double> raiz = sqrt(valores);
+    valarray<double> resta = valores - 10.0;
+    valarray<double> multiplicacion = valores * 10.0;
+    valarray<double> division = valores / 10.0;
+    valarray<double> potencia = pow(valores, 2);
+    valarray<double> logaritmo = log(valores);
+    valarray<double> exponencial = exp(valores);
+
+
+
 
 
 
